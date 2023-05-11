1936899ff25002c2d461d3465970670d6f5e30ab
yoonjoowon <wndnjs6224@naver.com>
2023-05-11 16:05:00 +0900

    feat : 북마크 수정

--- src/containerMy/Bookmark.js
+++ src/containerMy/Bookmark.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 function Bookmark() {
   const bookmarkList = useSelector((state) => state);
   console.log(bookmarkList)
+  
 
   return (
     <BookmarkStyle>
